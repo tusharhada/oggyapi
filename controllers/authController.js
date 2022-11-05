30ca2968e1c32aa10e8a740fba45b82b5166d49a
tusharhada <tusharhada99@gmail.com>
2022-11-05 10:33:26 +0530

    final commit: push to aws

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,28 @@
 import express from "express";
 const router = express.Router();
-import firebaseAdmin from 'firebase-admin';
+import firebaseAdmin from "firebase-admin";
 
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert('./cert.json')
-    // databaseURL: 'https://<my_project>.firebaseio.com'
-  });
+  credential: firebaseAdmin.credential.cert("./cert.json"),
+  // databaseURL: 'https://<my_project>.firebaseio.com'
+});
 
-export const getToken = async(req, res) =>{
+export const getToken = async (req, res) => {
+  const mob = req.body.mobile;
 
-    const mob = req.body.mobile
-    console.log(req.body);
-    console.log(mob.toString().length);
-    if (mob.toString().length == 10) {
-   
-
-      firebaseAdmin.auth().createCustomToken(mob.toString()).then(function(token) {
-        res.json({token: token});
-      }).
-      catch(function(error) {
-        res.status(500).json({error: "Error during token creation"});
-      });}
-      else {
-        res.status(500).json({error: "Invalid Mobile number"});
-      }
-}
+  if (mob.toString().length == 10) {
+    firebaseAdmin
+      .auth()
+      .createCustomToken(mob.toString())
+      .then(function (token) {
+        res.json({ token: token });
+      })
+      .catch(function (error) {
+        res.status(500).json({ error: "Error during token creation" });
+      });
+  } else {
+    res.status(500).json({ error: "Invalid Mobile number" });
+  }
+};
 
 export default router;
